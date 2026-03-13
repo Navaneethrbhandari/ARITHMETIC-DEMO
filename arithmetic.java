d72436a05308a7d4992f7a1507b32a40b1a2e4b9
Navaneethrbhandari <navaneethrbhandari@gmail.com>
2026-03-13 15:12:11 +0530

    Create arithmetic.java

---
+++ arithmetic.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+class arithmetic
+{
+  public static void main(String args[])
+  {
+    Scanner S=new Scanner(System.in)
+    System.out.println("enter a:");
+    int a=S.nextInt();
+    System.out.println("enter b:");
+    int b=S.nextInt();
+    int sum=a+b;
+    System.out.println("sum of two numbers:"+sum);
+  }
+}
+    
+    
